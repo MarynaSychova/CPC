f1a2945a740eab56bb77969a0dd3816263fd18cf
MarynaSychova <90452852+MarynaSychova@users.noreply.github.com>
2021-12-20 10:06:05 +0200

    Create 3_1_order.c

---
+++ 3_1_order.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Увести два числа і вивести їх у порядку зростання
+
+int main()
+{
+	int array[2];
+	
+	printf("Enter two numbers:\n");
+	for (int i = 0; i < 2; i++)
+	{
+		scanf("%i", array[i]);
+	}
+	
+	int max;
+	int min;
+	if (a > b)
+	{
+		printf("%i %i", b, a);
+	}
+	else if (a < b)
+	{
+		printf("%i %i", a, b);
+	}
+	else
+	{
+		printf("The numbers are equal.");
+	}	
+}
